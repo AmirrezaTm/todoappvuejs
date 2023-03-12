60a898d508539e33a6fd313d7ae8cda192e8ed5a
Amirreza <amirreza.tm86@gmail.com>
2023-03-12 14:59:37 -0400

    update mongodb

--- app.js
+++ app.js
@@ -17,12 +17,7 @@ app.use((req, res, next) => {
 
 app.use('/api/', routeUser)
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('connected to database')
     // listen to port
